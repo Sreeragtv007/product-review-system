9fc8a5ffe36f3dfa7848394591d693bc51dc3e3c
sreerag <sreeragtv007@gmail.com>
2025-07-13 10:19:47 +0530

    product model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def average_rating(self):
+        return self.reviews.aggregate(models.Avg('rating'))['rating__avg'] or 0
